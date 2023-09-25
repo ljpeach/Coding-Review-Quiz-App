66eb74a820efe205ad52e0839ea04f45504505e4
Liam Peachey <liamjpeachey@gmail.com>
2023-09-25 04:25:45 -0500

    Added code to display questions in random order with answers appearing in random order.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 var questions = [{
-    question: "",
-    answer: "",
-    incorrect: ["", "", ""]
+    question: "Question 1",
+    answer: "answer",
+    incorrect: ["wrong1", "wrong2", "wrong3"]
 },
 {
-    question: "",
-    answer: "",
-    incorrect: ["", "", ""]
+    question: "Question 2",
+    answer: "answer",
+    incorrect: ["wrong1", "wrong2", "wrong3"]
 },
 {
-    question: "",
-    answer: "",
-    incorrect: ["", "", ""]
+    question: "Question 3",
+    answer: "answer",
+    incorrect: ["wrong1", "wrong2", "wrong3"]
 },
 {
-    question: "",
-    answer: "",
-    incorrect: ["", "", ""]
+    question: "Question 4",
+    answer: "answer",
+    incorrect: ["wrong1", "wrong2", "wrong3"]
 }];
 
 // Get different html sections to switch between sections, and other vital elements
 var scoreLink = document.getElementById("score-link");
 
 var timerDisplay = document.getElementById("timer-display"); //span element that displays time left
-var countdown = 2; //default time for timer. Will likely change to 15*questions.length.
+var countdown = 60; //default time for timer. Will likely change to 15*questions.length.
 var timeLeft; //time left on timer.
 var timerInterval; //tracks the setIncrement call
 var timerPenalty = 10; //num of seconds penalized for incorrect answer
@@ -33,7 +33,7 @@ var startButton = document.getElementById("start-button");
 
 var questionSection = document.getElementById("question-box");
 var questionField = document.getElementById("question");
-var answerField = document.getElementById("answers");
+var answerField = document.getElementById("answers").querySelectorAll("li");
 
 var endSection = document.getElementById("end-screen");
 var scoreDisplay = document.getElementById("score-display");
@@ -48,10 +48,11 @@ var clearButton = document.getElementById("clear-button");
 //Other vital variables
 var activeFrame; //currently active section
 var highscores = []; //array of highscores
+var currentQuestion; //tracks current question.
 
 function intit() { //Start up tasks on site launch
     activeFrame = startSection;
-    questionSection.setAttribute("style", "display: none");
+    // questionSection.setAttribute("style", "display: none");
     endSection.setAttribute("style", "display: none");
     scoreSection.setAttribute("style", "display: none");
     changeActive(activeFrame); //display start on page
@@ -72,6 +73,10 @@ scoreLink.addEventListener("click", function () {
 })
 
 startButton.addEventListener("click", function () {
+    shuffleQuestions();
+    currentQuestion = 0;
+    renderQuestion();
+
     changeActive(questionSection);
     timeLeft = countdown;
     timerDisplay.textContent = timeLeft;
@@ -91,11 +96,24 @@ startButton.addEventListener("click", function () {
 
 // question list event listener, use event.target to determine whether correct index was chosen.
 
-function renderQuestion() { } //Display question
+function renderQuestion() {//Display question
+    questionField.textContent = questions[currentQuestion].question;
+    var ansOrder = [0, 1, 2, 3];
+    shuffle(ansOrder);
+    console.log(ansOrder);
+    for (var i = 0; i < answerField.length; i++) {
+        if (ansOrder[i] == 0) {
+            answerField[i].textContent = questions[currentQuestion].answer;
+        }
+        else {
+            answerField[i].textContent = questions[currentQuestion].incorrect[ansOrder[i] - 1];
+        }
+    }
+}
 
-function shuffleQuestions() { } //Reset order of questions in question list
-
-function shuffleAnswers() { } //randomize order answers appear
+function shuffleQuestions() { //Reset order of questions in question list
+    shuffle(questions);
+}
 
 // End screen related functions
 
@@ -124,5 +142,14 @@ function getScores() { } //get scores from local memory
 
 function addScore(initial, score) { } //Add new score to score array, sort.
 function renderScores() { } //render list of scores on high score page. 
+
+function shuffle(shuffleArr) {
+    for (var i = 0; i < shuffleArr.length; i++) {
+        sourceIndex = Math.floor(Math.random() * shuffleArr.length);
+        tempValue = shuffleArr[sourceIndex];
+        shuffleArr[sourceIndex] = shuffleArr[i];
+        shuffleArr[i] = tempValue;
+    }
+}
 
 intit();
